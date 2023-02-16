6284745f856e70deb8b55998fa6b7425ac3ae649
Andri Suvorov <andrisuvorov@gmail.com>
2023-02-16 17:52:48 +0200

    update to JS folder

--- JS/referenceTypes/arrays.js
+++ JS/referenceTypes/arrays.js
@@ -41,6 +41,19 @@ lotto;
 
 
 
+// * access nested arrays
+const floors = [
+    [ 'floor', '1' ],
+    [ 'floor', '2' ],
+    [ 'floor', '3' ]
+]
+
+// * access floor 3
+floors[2][1];
+// ? return value: "3"
+
+
+
 // TODO: ARRAY METHODS
 
 // * push - add item to the end of array
@@ -48,20 +61,28 @@ lotto.push(77);
 lotto;
 // ? return value: [44, 12, 22, 56, 66, 77]
 
+
+
 // * pop - remove item from the end of array
 lotto.pop();
 lotto;
 // ? return value: [44, 12, 22, 56, 66]
+
+
 
 // * unshift - add item to the beginning of array
 lotto.unshift(33);
 lotto;
 // ? return value: [33, 44, 12, 22, 56, 66]
 
+
+
 // * shift - remove item from the beginning of array
 lotto.shift();
 lotto;
 // ? return value: [44, 12, 33, 56, 66]
+
+
 
 // * concat - combine array with another array
 const array1 = ['data1', 'data2'];
@@ -70,12 +91,16 @@ const array2 = ['data3', 'data4'];
 const combinedArray = array1.concat(array2);
 // ? return value: ["data1", "data2", "data3", "data4"]
 
+
+
 // * includes - search for exact match in array
 lotto.includes(56);
 // ? return value: true
 
 lotto.includes(566);
 // ? return value: false
+
+
 
 // * indexOf - if match was found, return match index
 lotto.indexOf(56);
@@ -85,10 +110,14 @@ lotto.indexOf(3);
 // ? return value: -1
 // * value -1 means "not found"
 
+
+
 // * reverse - reverse array
 const word = ['D', 'O', 'G'];
 word.reverse();
 // ? return value: ["G", "O", "D"]
+
+
 
 // * join - join array
 const greet = ['H', 'E', 'L', 'L', 'O'];
@@ -98,6 +127,8 @@ greet.join();
 // * join can accept whatever character i give it to combine the array
 greet.join('');
 // ? return value: "HELLO"
+
+
 
 // * slice - cut from array
 const week = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
@@ -115,91 +146,39 @@ week.slice();
 
 
 
+// * splice - remove, add or replace items in array
+
+// ? myCollection is currently: [12, "dog", true, null, NaN];
+
+// * splice remove item
+// ? start index / delete item count
+myCollection.splice(2, 2);
+myCollection;
+// ? return value: [ 12, "dog", NaN ]
+
+// * splice add item
+// ? start index / delete item count / added value
+myCollection.splice(1, 0, 'new item');
+myCollection;
+// ? return value: [ 12, "new item", "dog", NaN ]
+
+// * splice replace item
+// ? start index / delete item count / added value
+myCollection.splice(0, 2, 'new item 1', 'new item 2');
+myCollection;
+// ? return value: [ "new item 1", "new item 2", "dog", NaN ]
 
 
-// Splice
 
-// removes or replaces elements, also can add in new elements
+// * sort - sort array
+const myLetters = ['C', 'E', 'B', 'A', 'D'];
 
-// insert method of splice()
-let webDevelopment = [
-    'HTML',
-    'CSS',
-    'Sass',
-    'Bootstrap',
-    'JavaScript'
-];
+/*
+   * "sort" usually places items alphabetically, but if we
+   * are dealing with numbers then "sort" converts them to
+   * strings and compares their numeric standing: 100, 1000, 10000 etc.
+*/
 
-webDevelopment.splice(1, 0, 'npm');
-// first num is start index ('HTML')
-// second num is how many items i want to delete
-// third value is the added value
-// return ["HTML", "npm", "CSS", "Sass", "Bootstrap", "JavaScript"]
-
-// delete method of splice()
-let webDevelopment = [
-    'HTML',
-    'CSS',
-    'Sass',
-    'Bootstrap',
-    'JavaScript'
-];
-
-webDevelopment.splice(2, 2);
-// first num is start index
-// second num is how many items i want to delete
-// return ["Sass", "Bootstrap"]
-
-// replace method of splice()
-let webDevelopment = [
-    'HTML',
-    'CSS',
-    'Sass',
-    'Bootstrap',
-    'JavaScript'
-];
-
-webDevelopment.splice(0, 2, 'HTML & CSS', 'Sass & SCSS');
-// first num is start index
-// second num is how many items i want to delete
-// return ["HTML", "CSS"]
-// and array is now ["HTML & CSS", "Sass & SCSS", "Sass", "Bootstrap", "JavaScript"]
-
-// --- --- --->
-
-// Sort
-
-// sort array alphabetically
-let webDevelopment = [
-    'HTML',
-    'CSS',
-    'Sass',
-    'Bootstrap',
-    'JavaScript'
-];
-
-webDevelopment.sort();
-// return ["Bootstrap", "CSS", "HTML", "JavaScript", "Sass"]
-
-// sorting with numbers (converts to string and compares numeric standing)
-let nums = [35, 100, 10000, 66, 99];
-
-nums.sort();
-// return [100, 10000, 35, 66, 99]
-
-// --- --- --->
-
-// Access nested arrays
-
-const floors = [
-    [ 'floor', '1' ],
-    [ 'floor', '2' ],
-    [ 'floor', '3' ]
-]
-
-// access floor 3
-floors[2][1]
-// return '3'
-
-// update value
-floors[2][1] = 4 
+// * alphabetical sort
+myLetters.sort();
+// ? return value: [ "A", "B", "C", "D", "E" ]
